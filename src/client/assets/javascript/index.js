ca65aa2bf5668e17ea559e8987f7ee473540f135
mokajima <mokajima85z@gmail.com>
2020-10-01 12:09:05 +0900

    feat: Invoke the API call to accelerate

--- src/client/assets/javascript/index.js
+++ src/client/assets/javascript/index.js
@@ -183,7 +183,9 @@ function handleSelectTrack(target) {
 
 function handleAccelerate() {
 	console.log("accelerate button clicked")
-	// TODO - Invoke the API call to accelerate
+
+  // Invoke the API call to accelerate
+  accelerate(store.race_id)
 }
 
 // HTML VIEWS ------------------------------------------------
